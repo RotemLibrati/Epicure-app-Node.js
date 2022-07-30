945a553fcf5a7c94b3ca3874052bb5abe8c1df82
RotemLibrati <rotemlibrati1@gmail.com>
2022-07-30 14:38:46 +0300

    Fix the wrong at image

--- models/Chef.js
+++ models/Chef.js
@@ -5,7 +5,7 @@ const ChefSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    imgae: { 
+    image: { 
         type: String,
         required: true,
         default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
